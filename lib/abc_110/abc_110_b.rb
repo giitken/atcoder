3f1c1896a217bed2de0a128baeac279aed9b2cbc
ken <kirie115@gmail.com>
2019-03-02 09:02:51 +0900

    [solved]abc-110b(元ファイルはabc-113に格納)

--- lib/abc_110/abc_110_b.rb
+++ lib/abc_110/abc_110_b.rb
@@ -1,27 +1,5 @@
-N = gets #使わない
-T, HEIGHT = gets.split.map(&:to_i)
-CANDIDATES = gets.split.map(&:to_i)
-ADJUSTING_VALUE = 0.006
-
-class Finder
-  def run(candidates)
-    average_temp = calc_average_temperature(candidates)
-    difference = calc_approx_val(average_temp)
-    find_point_to_build(difference)
-  end
-
-  private
-  def calc_average_temperature(array)
-    array.map { |x| T - x * ADJUSTING_VALUE}
-  end
-
-  def calc_approx_val(difference)
-    difference.map{|ave_temp| (HEIGHT - ave_temp).abs}
-  end
-
-  def find_point_to_build(approx_val)
-    approx_val.index(approx_val.min) + 1
-  end
-end
-
-puts Finder.new.run(CANDIDATES)
+N, M, X, Y = gets.chomp.split(" ").map(&:to_i)
+x, y = readlines.map{|a| a.split(" ").map(&:to_i)}
+max_x_coord = x.max
+min_y_coord = y.min
+puts max_x_coord < min_y_coord && X < min_y_coord && max_x_coord < Y ? "No War" : "War"
