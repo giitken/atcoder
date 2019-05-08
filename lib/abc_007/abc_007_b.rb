646e61cbf002cd4949fe08bb665e44d745f6c774
ken3ypa <kirie115@gmail.com>
2019-05-08 19:18:01 +0900

    Add abc_007-b

--- lib/abc_007/abc_007_b.rb
+++ lib/abc_007/abc_007_b.rb
@@ -1,2 +1,10 @@
-puts gets.chomp == "a" ? -1 : "a"
+n = gets.to_i
+a = [0, 0, 1]
+ 
+n.times do |i|
+  next if i < 3
+  a.push (a[i - 1] + a[i - 2] + a[i - 3]) % 10007
+end
+ 
+p a[n - 1]puts gets.chomp == "a" ? -1 : "a"
 
